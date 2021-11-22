f6057d50e8bf10aa6f2ed9ca28a5ea315427b0d2
Catherine Clague <catherine.clague@trademe.co.nz>
2021-11-23 11:46:12 +1300

    File setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Hangman
+{
+    static void Main()
+    {
+        string mainWord = "Funky";
+        
+        Console.WriteLine("Select a letter");
+        char userInput = Console.ReadKey().KeyChar;
+        
+        Console.WriteLine();
+        Console.WriteLine("You picked '" + userInput + "'");
+    }
+}
