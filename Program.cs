72810809652b64e6c68d5585ff0d4a6a56d9f5ec
Catherine Clague <catherine.clague@trademe.co.nz>
2022-02-01 09:43:49 +1300

    Loop and lower case

--- Program.cs
+++ Program.cs
@@ -4,12 +4,34 @@ class Hangman
 {
     static void Main()
     {
-        string mainWord = "Funky";
-        
-        Console.WriteLine("Select a letter");
-        char userInput = Console.ReadKey().KeyChar;
-        
-        Console.WriteLine();
-        Console.WriteLine("You picked '" + userInput + "'");
+        string mainWord = "funky";
+        int numFailed = 0;
+
+
+        while (true)
+        {
+            Console.WriteLine("Select a letter");
+            char userInput = Console.ReadKey().KeyChar;
+            char userLetter = Char.ToLower(userInput);
+
+            Console.WriteLine();
+            Console.WriteLine("You picked '" + userInput + "'");
+
+            ValuateChar(userLetter, mainWord, ref numFailed);
+        }
+    }
+
+    static void ValuateChar(char letter, string mainWord, ref int numFailed)
+    {
+        if (mainWord.Contains(letter))
+        {
+            Console.WriteLine("Does contain");
+        }
+        else
+        {
+            Console.WriteLine("Does not contain");
+            numFailed++;
+            Console.WriteLine("Failed - " + numFailed);
+        }
     }
 }
